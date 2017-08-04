ee4009bfd56bd1c1543f3f55f58cc5264c214b51
mdarficops <arabic91@hotmail.fr>
2017-08-04 22:53:46 +0200

    Fist commit, adding sources

---
+++ src/main/java/fr/mdarfilal/JaxWsInitiationApplication.java
@@ -0,0 +1,12 @@
+package fr.mdarfilal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaxWsInitiationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaxWsInitiationApplication.class, args);
+	}
+}
---
+++ src/test/java/fr/mdarfilal/JaxWsInitiationApplicationTests.java
@@ -0,0 +1,16 @@
+package fr.mdarfilal;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JaxWsInitiationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
